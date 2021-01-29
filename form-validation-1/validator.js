67a3782a1824f2f636087c5ba9c98ce8fc801b82
hahunavth <66118664+hahunavth@users.noreply.github.com>
2021-01-29 22:53:17 +0700

    Add files via upload

---
+++ form-validation-1/validator.js
@@ -0,0 +1,143 @@
+function Validator(option) {
+  var formElement = document.querySelector(option.form);
+  var selectorRules = {};
+
+  if (formElement) {
+    formElement.onsubmit = function (e) {
+      e.preventDefault();
+
+      var isValid = true;
+
+      option.rules.forEach(function (rule) {
+        if (Array.isArray(selectorRules[rule.selector])) {
+          selectorRules[rule.selector].push(rule.test);
+        } else {
+          selectorRules[rule.selector] = [rule.test];
+        }
+        var inputElement = formElement.querySelector(rule.selector);
+
+        var rules = selectorRules[rule.selector];
+        var formMessage = inputElement.parentElement.querySelector(
+          option.errorSelector
+        );
+
+        for (var i = 0; i < rules.length; i++) {
+          var errorMessage = rules[i](inputElement.value);
+          if (errorMessage) {
+            isValid = false;
+            break;
+          }
+        }
+
+        if (errorMessage) {
+          formMessage.innerText = errorMessage;
+          inputElement.classList.add("invalid");
+        } else {
+          inputElement.classList.remove("invalid");
+          formMessage.innerText = "";
+        }
+      });
+
+      if (isValid) {
+        if (typeof option.onSubmit === "function") {
+          var enableInputs = formElement.querySelectorAll(
+            "[name]:not([disabled])"
+          );
+
+          var formValue = Array.from(enableInputs).reduce(function (
+            values,
+            input
+          ) {
+            return (values[input.name] = input.value) && values;
+          },
+          {});
+
+          option.onSubmit(formValue);
+        } else {
+          formElement.submit();
+        }
+      }
+    };
+
+    option.rules.forEach(function (rule) {
+      if (Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector].push(rule.test);
+      } else {
+        selectorRules[rule.selector] = [rule.test];
+      }
+
+      var inputElement = formElement.querySelector(rule.selector);
+      if (inputElement)
+        inputElement.onblur = function () {
+          var errorMessage;
+          var formMessage = inputElement.parentElement.querySelector(
+            option.errorSelector
+          );
+          var rules = selectorRules[rule.selector];
+
+          for (var i = 0; i < rules.length; i++) {
+            var errorMessage = rules[i](inputElement.value);
+            if (errorMessage) break;
+          }
+
+          if (errorMessage) {
+            formMessage.innerText = errorMessage;
+            inputElement.classList.add("invalid");
+          } else {
+            inputElement.classList.remove("invalid");
+            formMessage.innerText = "";
+          }
+
+          inputElement.oninput = function () {
+            inputElement.classList.remove("invalid");
+            formMessage.innerText = "";
+          };
+        };
+    });
+  }
+}
+
+Validator.isRequired = function (selector) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value.trim() ? undefined : "Vui lòng nhập trường này";
+    },
+  };
+};
+
+Validator.isEmail = function (selector) {
+  return {
+    selector: selector,
+    test: function (value) {
+      var email = value.trim();
+      function validateEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+      }
+      return validateEmail(email) ? undefined : "Trường này phải là email";
+    },
+  };
+};
+
+Validator.minLength = function (selector, min) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value.length >= 6
+        ? undefined
+        : "Vui lòng nhập ít nhất " + min + " ký tự";
+    },
+  };
+};
+
+Validator.isConfirm = function (selector, getConfirmValue, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value === getConfirmValue()
+        ? undefined
+        : message || "Gía trị nhập vào không chính xác";
+    },
+  };
+};
